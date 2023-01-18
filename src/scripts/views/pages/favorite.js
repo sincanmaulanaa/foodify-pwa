241dc7cf39f9afd82b769c3cde404d0fa056c2c3
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-18 14:08:24 +0700

    create favorite page to render item favorited

---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-tabs */
+const Favorite = {
+  async render() {
+    return `
+	<section class="main_title">
+		<h2>Favorite Restaurants</h2>
+	</section>
+		`;
+  },
+
+  async afterRender() {},
+};
+
+export default Favorite;
