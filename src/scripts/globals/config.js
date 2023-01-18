4aa03005e3d0559b8e941c8abe740a60ee0472cc
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-18 14:06:06 +0700

    create config file to store base url and base image url

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+/* eslint-disable implicit-arrow-linebreak */
+const CONFIG = {
+  BASE_URL: 'https://restaurant-api.dicoding.dev',
+  BASE_IMAGE_URL: (size) =>
+    `https://restaurant-api.dicoding.dev/images/${size}/`,
+};
+
+export default CONFIG;
