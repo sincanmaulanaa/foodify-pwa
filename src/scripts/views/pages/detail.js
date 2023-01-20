410c8d04bc39780943ac3bc303d3001fa4ec3f49
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-21 03:11:16 +0700

    initiate like button initiator object

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,6 +1,7 @@
 import RestaurantSource from '../../data/restaurant-source';
 import UrlParser from '../../routes/url-parser';
 import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 import CONFIG from '../../globals/config';
 
 /* eslint-disable no-tabs */
@@ -9,6 +10,7 @@ const Detail = {
     return `
 	<section class="content">
 		<div class="restaurant" id="restaurant"></div>
+    <div id="likeButtonContainer"></div>
 	</section>
 		`;
   },
@@ -18,13 +20,24 @@ const Detail = {
     const restaurant = await RestaurantSource.detailRestaurant(url.id);
     const restaurantContainer = document.querySelector('#restaurant');
     const heroImageContainer = document.querySelector('#hero');
-
     const restaurantImage = `<img id="restaurant-image" class="restaurant_image" src="${
       CONFIG.BASE_IMAGE_URL('medium') + restaurant.pictureId
     }" alt="${restaurant.name}" />`;
 
     heroImageContainer.innerHTML = restaurantImage;
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: restaurant.id,
+        name: restaurant.name,
+        description: restaurant.description,
+        pictureId: restaurant.pictureId,
+        city: restaurant.city,
+        rating: restaurant.rating,
+      },
+    });
   },
 };
 
