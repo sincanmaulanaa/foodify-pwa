de5ee6a7f2724889d6efbe6c9ffdcd551eaa7c90
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-21 03:11:57 +0700

    create templates for like button

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable quotes */
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
@@ -80,4 +81,21 @@ const createRestaurantItemTemplate = (restaurant) => `
 </article>
 `;
 
-export { createRestaurantDetailTemplate, createRestaurantItemTemplate };
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this restaurant" id="likeButton" class="like">
+     <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
+`;
+
+const createLikedButtonTemplate = () => `
+  <button aria-label="unlike this restaurant" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
+export {
+  createRestaurantDetailTemplate,
+  createRestaurantItemTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
+};
