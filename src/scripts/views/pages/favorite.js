9f2e3453275f3ab78da717aa28f82d6965deeea9
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-21 03:11:40 +0700

    update render and afterRender methods

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,14 +1,30 @@
 /* eslint-disable no-tabs */
+/* eslint-disable operator-linebreak */
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+import { createRestaurantItemTemplate } from '../templates/template-creator';
+
 const Favorite = {
   async render() {
     return `
-	<section class="main_title">
-		<h2>Favorite Restaurants</h2>
-	</section>
-		`;
+	  <section class="main_title">
+		 <p>Your</p>
+		 <h2>Favorite Restaurants</h2>
+	   </section>
+       <section class="content">
+      	 <div class="card" id="card"></div>
+       </section>
+    `;
   },
 
-  async afterRender() {},
+  async afterRender() {
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+    const restaurantsContainer = document.querySelector('#card');
+
+    restaurants.forEach((restaurant) => {
+      restaurantsContainer.innerHTML +=
+        createRestaurantItemTemplate(restaurant);
+    });
+  },
 };
 
 export default Favorite;
