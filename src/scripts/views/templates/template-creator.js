17c3b4b98e1046eb44b013a87c32320e815e43d9
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-01-20 19:20:04 +0700

    update restaurant-detail template

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
 /* eslint-disable implicit-arrow-linebreak */
@@ -5,10 +6,59 @@ import CONFIG from '../../globals/config';
 
 const createRestaurantDetailTemplate = (restaurant) =>
   `
-	<h2 class="restaurant_name">${restaurant.name}</h2>
-	<div class="restaurant_info">
-		<p class="city">${restaurant.city}</p>
-		<p class="rating">⭐ ${restaurant.rating}</p>
+	<div class="restaurant_detail_container">
+		<h2 class="restaurant_name">${restaurant.name}</h2>
+		<div class="restaurant_info">
+			<p class="city">${restaurant.city}</p>
+			<p class="rating">⭐ ${restaurant.rating}</p>
+		</div>
+		<div class="restaurant_detail">
+			<p>Address</p>
+			<p class="address">${restaurant.address}, ${restaurant.city}</p>
+			<p>Category</p>
+			<ul class="category">${restaurant.categories
+        .map((category) => `<li class="category_item">${category.name}</li>`)
+        .join('|')}</ul>
+		</div>
+		<div class="restaurant_menus">
+			<h3>Menus</h3>
+			<div class="menus_container">
+				<div class="foods">
+					<h4>Foods</h4>
+					<ul class="foods">
+						${restaurant.menus.foods
+              .map((food) => `<li class="food_item">${food.name}</li>`)
+              .join('')}
+					</ul>
+				</div>
+				<div class="drinks">
+					<h4>Drinks</h4>
+						<ul class="drinks">
+						${restaurant.menus.drinks
+              .map((drink) => `<li class="drink_item">${drink.name}</li>`)
+              .join('')}
+					</ul>		
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="customer_reviews">
+		<h3 class="customer_reviews_title">
+			What People Say
+		</h3>
+		<div class="customer_reviews_list">
+			${restaurant.customerReviews
+        .map(
+          (review) => `
+					<div class="customer_review_item">
+						<span class="customer_name">${review.name}</span>
+						<p class="customer_review">${review.review}</p>
+						<span class="customer_date">${review.date}</span>
+					</div>
+					`
+        )
+        .join('')}
+		</div>
 	</div>
 	<div class="restaurant_description">
 		<h3>About Us</h3>
